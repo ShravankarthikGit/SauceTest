51298f3c53132c4bfebd244f672a0a55605d273c
ShravankarthikGit <44978093+ShravankarthikGit@users.noreply.github.com>
2020-08-20 11:39:54 -0700

    Update testDataUpload.bat

--- testDataUpload.bat
+++ testDataUpload.bat
@@ -1,1 +1,1 @@
-bitsadmin.exe /transfer "JobName" C:\Users\886257\OneDrive - American Airlines, Inc\Desktop\AA\SauceLabs\Sampletestdata.txt C:\Users\Administrator\Desktop\Sampletestdata.txt
+bitsadmin.exe /transfer "JobName" %UserProfile%//SeleniumSauceLabsGHE//Flights.txt C:\Users\Administrator\Downloads\Flights.txt
